56bd1ddd5421666797c036b5525cd1106dec77f3
Dino0204 <s24020@gsm.hs.kr>
2024-12-21 20:08:13 +0900

    💄Feat: Tailwind CSS & login style

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,24 +1,32 @@
 import React from "react";
-import { auth, provider } from '../firebase.js';
+import { auth, provider } from "../firebase.js";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import GoogleLogo from "../../public/google.svg";
+import ArimasLogo from "../../public/logoWithTitle.svg";
 
 export default function Login() {
-
   const navigate = useNavigate();
 
   const LoginGoogle = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
-      navigate('/');
-    } catch(error) {
+      navigate("/");
+    } catch (error) {
       console.error("로그인 중 에러 발생했어요 !!", error);
     }
-  }
+  };
 
   return (
-    <>
-      <button onClick={LoginGoogle}>Google로 로그인 하기</button>
-    </>
+    <div className="flex w-full h-full justify-center">
+      <img src={ArimasLogo} />
+      <button
+        onClick={LoginGoogle}
+        className="flex justify-center w-full max-w-md items-center border border-[#FFBA00] py-6 rounded-2xl"
+      >
+        <img src={GoogleLogo} />
+        구글로 로그인
+      </button>
+    </div>
   );
 }
